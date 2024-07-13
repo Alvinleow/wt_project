5c4bac02787b6690a063f9ef91ffb606b7113d05
ColdIce0816 <alvinleowyankai@gmail.com>
2024-07-13 17:36:31 +0800

    Updated Code

--- back-end/routes/course.js
+++ back-end/routes/course.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const courseController = require("../controllers/courseController");
 const multer = require("multer");
 const upload = multer({ storage: multer.memoryStorage() });
-
-// Create course
-router.post("/", upload.single("thumbnail"), courseController.createCourse);
+const courseController = require("../controllers/courseController");
 
 // Get all courses
 router.get("/", courseController.getAllCourses);
 
-// Get a single course by _id
+// Get a single course by ID
 router.get("/:id", courseController.getCourseByID);
+
+// Create new course
+router.post("/", upload.single("thumbnail"), courseController.createCourse);
 
 // Update course
 router.put("/:id", upload.single("thumbnail"), courseController.updateCourse);
